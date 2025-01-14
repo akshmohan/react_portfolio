edbd9df7430cac9c760333657fe3229e92e304ca
akshmohan <145959795+akshmohan@users.noreply.github.com>
2025-01-14 10:38:24 +0530

    Added download resume button

---
+++ src/components/DownloadResume.jsx
@@ -0,0 +1,31 @@
+
+const DownloadResume = () => {
+    const resumeUrl = 'src/assets/resume/Resume.pdf';
+  
+    return (
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <a
+          href={resumeUrl}
+          download="AkshMohan_Resume.pdf"
+          style={{
+            textDecoration: 'none',
+            color: '#fff',
+            backgroundColor: '#007BFF',
+            padding: '12px 20px',
+            borderRadius: '5px',
+            fontWeight: 'bold',
+            display: 'inline-block',
+            fontSize: '16px',
+            marginTop: '20px',
+            transition: 'background-color 0.3s ease',
+          }}
+          onMouseEnter={(e) => (e.target.style.backgroundColor = '#0056b3')}
+          onMouseLeave={(e) => (e.target.style.backgroundColor = '#007BFF')}
+        >
+          Download My Resume
+        </a>
+      </div>
+    );
+  };
+  
+  export default DownloadResume;
